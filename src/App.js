1305c1bce453277673515b18bd7de11b1e20289b
wiembg <bengamrawiem1230@gmail.com>
2022-07-24 16:56:35 +0200

    1:12

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 
-import './App.css';
+import { useStateContext } from './contexts/ContextProvider';
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 const App = () => {
-  return(
-    <h1 className="underline text-3xl">Hello World</h1>
-  )
-}
+  //const activeMenu=true;
+  const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings } = useStateContext();
+  return (
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+     
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                style={{ background: "blue", borderRadius: '50%' }}//setting colorbutton 
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+
+          </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+               <Sidebar/>
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+                  {/* it s not gonna be visible web:0*/}
+                  <Sidebar/>
+            </div>
+          )}
+
+<div  className={`dark:bg-main-dark-bg  bg-main-bg min-h-screen${activeMenu ? "w-72" : "w-0"}`}>
+<div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full "> navbar  </div>    </div>    
+                    
+           
+          
+        
+        </div>
+
+
+       {/*  <div>
+        <Routes>
+          <Route path="/" element="home" />
+          <Route path="/about" element="about" />
+          </Routes>
+          </div> */}
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-
+import { ContextProvider } from './contexts/ContextProvider';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+     <ContextProvider>
     <App />
+    </ContextProvider>
   </React.StrictMode>
 );
 
